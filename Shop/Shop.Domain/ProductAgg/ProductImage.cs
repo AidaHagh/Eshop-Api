b0bc2862cb2d356fe0989ca1692e0abd9d565da7
aida <aida1992.h@gmail.com>
2024-04-24 15:35:28 +0330

    Complete ProductAggregate

---
+++ Shop/Shop.Domain/ProductAgg/ProductImage.cs
@@ -0,0 +1,24 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.ProductAgg
+{
+    public class ProductImage : BaseEntity
+    {
+        public ProductImage(string imageName, int image_Order)
+        {
+            NullOrEmptyDomainDataException.CheckString(imageName, nameof(imageName));   
+            ImageName = imageName;
+            Image_Order = image_Order;
+        }
+
+        public long ProductId { get; internal set; }
+        public string ImageName { get; private set; }
+        public int Image_Order { get; private set; }
+    }
+}
---
+++ Shop/Shop.Domain/ProductAgg/ProductSpecification.cs
@@ -0,0 +1,27 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.ProductAgg
+{
+    public class ProductSpecification : BaseEntity
+    {
+        public ProductSpecification( string key, string value)
+        {
+            NullOrEmptyDomainDataException.CheckString(value, nameof(value));   
+            NullOrEmptyDomainDataException.CheckString(key, nameof(key));
+            Key = key;
+            Value = value;
+        }
+
+        public long ProductId { get;internal set; }
+        public string Key { get; private set; }
+        public string Value { get; private set; }
+
+    }
+}
