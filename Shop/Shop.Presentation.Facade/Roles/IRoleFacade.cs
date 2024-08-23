cc4d7f171aededc15108cc913428400655f03eac
aida <aida1992.h@gmail.com>
2024-08-23 20:26:43 +0330

    Add Roles in Presentation.Facade Layer

--- Shop/Shop.Presentation.Facade/Roles/IRoleFacade.cs
+++ Shop/Shop.Presentation.Facade/Roles/IRoleFacade.cs
@@ -1,15 +1,22 @@
-﻿using System;
+﻿using Common.Application;
+using Shop.Application.Roles.Create;
+using Shop.Application.Roles.Edit;
+using Shop.Query.Roles.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Shop.Presentation.Facade.Roles;
 
 public interface IRoleFacade
 {
-}   
+    Task<OperationResult> CreateRole(CreateRoleCommand command);
+    Task<OperationResult> EditRole(EditRoleCommand command);
 
-internal class RoleFacade : IRoleFacade
-{
+
+    Task<RoleDto?> GetRoleById(long id);
+    Task <List<RoleDto>> GetRoles();
 }
